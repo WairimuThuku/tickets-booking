c89bb44e7ecb03ae4abf3ecba4c42365bb378492
WairimuThuku <lucythuku33@gmail.com>
2022-02-27 17:38:01 +0300

    creating of the home page

--- app/src/main/java/com/myapp/ticketsbooking/MainActivity.java
+++ app/src/main/java/com/myapp/ticketsbooking/MainActivity.java
@@ -2,13 +2,26 @@ package com.myapp.ticketsbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button search_airline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        search_airline = findViewById(R.id.search_airline);
+        search_airline.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+            startActivity(intent);
+        });
     }
 }
+
